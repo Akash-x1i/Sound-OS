7c30ff2903b4ad82c37a8e17bc3dad9339d2dfca
Akash Bauri <108396971+Akash-x1i@users.noreply.github.com>
2022-08-08 10:29:33 +0530

    interfacing pseudo code development.

---
+++ Interfacing.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using std as namespace;
+int cursor;
+
+int fw(cursor) {
+    cursor = cursor+1;
+
+    return cursor
+}
+
+int bw(cursor) {
+     cursor = cursor-1
+
+     return cursor
+}
+int ent(cursor) {
+
+}
+int main() {
+    analog_read d1 ;
+    
+}
